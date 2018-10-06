5c92c060209833eddf9a994e97b509b0b140da69
Phil Kunz <github@philkunz.com>
2018-10-06 17:51:53 +0200

    fix(typings): fix compilation problem

--- ts/index.ts
+++ ts/index.ts
@@ -1,1 +1,9 @@
+// compatibility
+declare global {
+  interface Element { }
+  interface Node { }
+  interface NodeListOf<TNode = Node> { }
+}
+
+// normal
 export * from './smartpdf.classes.smartpdf';
